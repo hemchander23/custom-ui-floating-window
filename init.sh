555b2b6f7441ee7d7ad6dc85e57c83709549ded5
Hem <hemchander.sundaravel@freshworks.com>
2023-03-01 13:33:30 +0000

    Configuration complete

--- init.sh
+++ init.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-echo ""
 tput setaf 3
-echo ""
-echo "Auth Callback URL: "
+echo -e "\n1️⃣ OAuth Callback URL: \n"
 tput setaf 7
-echo "$(gp url 3000)/api/auth/callback"
-echo ""
+echo -e "$(gp url 3000)/api/auth/callback"
 tput setaf 3
-echo "Floating Window URL: "
+echo -e "\n2️⃣ Floating Window URL: \n"
 tput setaf 7
-echo "$(gp url 3000)/"
-echo ""
-echo "Make sure you fill in the `.env` file based on `.env.example`. Run the app using `npm run dev` command."
-
-
+echo -e "$(gp url 3000)/"
+tput setaf 3
+echo -e "\n3️⃣ Make sure you fill in the '.env' file based on '.env.example'. Hit enter once done to start the app in dev mode...\n"
+read 
+tput setaf 7
+npm run start
